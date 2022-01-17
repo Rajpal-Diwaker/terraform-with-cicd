2449af26243e3062f9e76e2d35ea5259cf0ab366
Rajpal-Diwaker <rajpaldiwaker@gmail.com>
2022-01-18 01:40:30 +0530

    15 commit

--- examples/main.tf
+++ examples/main.tf
@@ -12,5 +12,8 @@ module "kylo_ren" {
   source           = "../modules/gce"
   suffix           = "local.suffix"
   gcp_project_id   = var.gcp_project_id
+  vpc_network_name = "default"
+  instance_name    = "kylo-ren"
+  network_tags     = ["http-server", "https-server"]
 }
 
--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -3,7 +3,14 @@ terraform {
 }
 
 locals {
-  sa_id = format("%s-sa-%s", "var.instance_name", "var.suffix")
+  instance_name = format("%s-%s", var.instance_name, var.suffix)
+  region        = data.google_client_config.google_client.region
+  zone          = format("%s-%s", local.region, var.zone)
+  network_tags  = tolist(toset(var.network_tags))
+
+  name_static_vm_ip = format("%s-ext-ip-%s", var.instance_name, var.suffix)
+
+  sa_id = format("%s-sa-%s", var.instance_name, var.suffix)
 }
 
 resource "google_project_service" "compute_api" {
@@ -25,9 +32,43 @@ resource "google_service_account" "gce_sa" {
   }
 }
 
-resource "google_project_iam_member" "spanner_role" {
-  role   = "roles/spanner.viewer"
-  member = "serviceAccount:${google_service_account.gce_sa.email}"
+
+resource "google_compute_instance" "gce" {
+  project      = var.gcp_project_id
+  name         = local.instance_name
+  machine_type = var.instance_machine_type
+  zone         = local.zone
+  tags         = local.network_tags
+
+  boot_disk {
+    initialize_params {
+      size  = var.boot_disk_size
+      type  = var.boot_disk_type
+      image = var.boot_disk_image
+    }
+  }
+
+  network_interface {
+    network = var.vpc_network_name
+    access_config {
+      nat_ip       = google_compute_address.gce_static_ip.address
+      network_tier = "PREMIUM"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      attached_disk,
+    ]
+  }
+ 
+  depends_on = [google_project_service.compute_api]
+
+  timeouts {
+    create = var.vm_instance_timeout
+    update = var.vm_instance_timeout
+    delete = var.vm_instance_timeout
+  }
 }
 
 data "google_client_config" "google_client" {}
