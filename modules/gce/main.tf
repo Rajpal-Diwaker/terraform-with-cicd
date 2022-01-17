7f11ed434f5e260b97c8e6bfb00adeb79ff14646
Rajpal-Diwaker <rajpaldiwaker@gmail.com>
2022-01-18 01:10:39 +0530

    9 commit

--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -13,6 +13,15 @@ locals {
   sa_id = format("%s-sa-%s", var.instance_name, var.suffix)
 }
 
+resource "google_project_service" "compute_api" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "networking_api" {
+  service            = "servicenetworking.googleapis.com"
+  disable_on_destroy = false
+}
 
 resource "google_service_account" "gce_sa" {
   account_id   = local.sa_id
