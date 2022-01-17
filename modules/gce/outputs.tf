e6cace0b3894c0e0affc98d3968ace0d36181090
Rajpal-Diwaker <rajpaldiwaker@gmail.com>
2022-01-18 01:15:36 +0530

    10 commit

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -13,3 +13,22 @@ output "instance_id" {
   description = "The server-assigned unique identifier of this instance. Example: 4567719474035761998"
 }
 
+output "self_link" {
+  value       = google_compute_address.gce_static_ip.self_link
+  description = "The URI of the External Static IP resource."
+}
+
+output "email" {
+  value       = google_service_account.gce_sa.email
+  description = "The URI of the Google Service Account resource."
+}
+
+output "sa_id" {
+  value       = local.sa_id
+  description = "Display Name of created Google Service Account"
+}
+
+output "region" {
+  value       = data.google_client_config.google_client.region
+  description = "GCP Region in which GCE VM Instance being created"
+}
